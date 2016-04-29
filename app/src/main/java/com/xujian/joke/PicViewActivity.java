8e5ce71262c9bc566bda8e1aa287e7fd0891c935
White <xj626361950@163.com>
2016-04-29 15:48:43 +0800

    添加图片放大和缩小功能，

--- app/src/main/java/com/xujian/joke/PicViewActivity.java
+++ app/src/main/java/com/xujian/joke/PicViewActivity.java
@@ -1,10 +1,13 @@
 package com.xujian.joke;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
 import com.facebook.drawee.drawable.ScalingUtils;
 import com.xj.frescolib.View.FrescoDrawee;
+import com.xujian.joke.Utils.FrescoBitmapUtils;
+import com.xujian.joke.Utils.ImageTouchView;
 
 /**
  * 图片查看
@@ -12,6 +15,7 @@ import com.xj.frescolib.View.FrescoDrawee;
 public class PicViewActivity extends BaseActivity {
     private FrescoDrawee picView;
     private String  picUrl;
+    private ImageTouchView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +26,36 @@ public class PicViewActivity extends BaseActivity {
 
     private void initView(){
         picView = (FrescoDrawee)findViewById(R.id.picture_view);
+        image = (ImageTouchView) findViewById(R.id.image);
         picUrl = getIntent().getStringExtra("url");
-        picView.setImageURI(picUrl);
-        picView.setImageImageScaleType(ScalingUtils.ScaleType.FIT_CENTER);
-        picView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        if (picUrl.contains(".gif")){
+            picView.setVisibility(View.VISIBLE);
+            image.setVisibility(View.GONE);
+            picView.setImageURI(picUrl);
+            picView.setImageImageScaleType(ScalingUtils.ScaleType.FIT_CENTER);
+            picView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }else{
+            image.setVisibility(View.VISIBLE);
+            picView.setVisibility(View.GONE);
+            FrescoBitmapUtils.getImageBitmap(PicViewActivity.this, picUrl, new FrescoBitmapUtils.BitCallBack() {
+                @Override
+                public void onNewResultImpl(final Bitmap bitmap) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            image.setImageBitmap(bitmap);
+                        }
+                    });
+                }
+            });
+
+        }
+
     }
 
 
